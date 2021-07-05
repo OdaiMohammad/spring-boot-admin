874c6e7a4099b5330670ed719384017f25417632
Odai Mohammad <omohammad@accumedsy.ae>
2021-07-05 11:24:11 +0300

    Add Spring Boot Admin server

---
+++ src/test/java/ae/accumed/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ae.accumed.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
